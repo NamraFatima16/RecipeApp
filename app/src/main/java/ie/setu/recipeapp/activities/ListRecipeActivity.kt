05b2823b9eeeb2f92d1a2439cc8c239671b6e241
Namra <20094326@mail.wit.ie>
2023-03-02 16:32:31 +0000

    Listing recipe and making RecipeAdapter

--- app/src/main/java/ie/setu/recipeapp/activities/ListRecipeActivity.kt
+++ app/src/main/java/ie/setu/recipeapp/activities/ListRecipeActivity.kt
@@ -1,8 +1,59 @@
 package ie.setu.recipeapp.activities
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ie.setu.recipeapp.R
+import ie.setu.recipeapp.databinding.ActivityListRecipeBinding
+import ie.setu.recipeapp.databinding.CardRecipeBinding
+import ie.setu.recipeapp.main.MainApp
+import ie.setu.recipeapp.models.RecipeModel
 
 class ListRecipeActivity : AppCompatActivity(){
 
+ lateinit var app: MainApp
+ private lateinit var binding: ActivityListRecipeBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListRecipeBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_list_recipe)
+
+        app = application as MainApp
+
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = RecipeAdapter(app.recipes)
+    }
+}
+
+class RecipeAdapter constructor(private var recipes: List<RecipeModel>) :
+    RecyclerView.Adapter<RecipeAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardRecipeBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val recipe = recipes[holder.adapterPosition]
+        holder.bind(recipe)
+    }
+
+    override fun getItemCount(): Int = recipes.size
+
+    class MainHolder(private val binding : CardRecipeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(recipe: RecipeModel) {
+            binding.recipeTitle.text = recipe.title
+
+        }
+    }
 }
